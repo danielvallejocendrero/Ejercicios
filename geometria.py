ccd21e0fc3f2b9369cf14e07f3354a6308b68389
Daniel Vallejo Cendrero <d.vallejo.2018@alumnos.urjc.es>
2019-02-11 16:34:58 +0100

    V2

--- geometria.py
+++ geometria.py
@@ -10,7 +10,10 @@ class Cuadrado(Figura):
         self.lado = lado
     def calcular_area(self):
         return self.lado*2
+	def calcular_perim(self):
+		return self.lado*4
         
 
 cuadrado = Cuadrado(4)
 print(cuadrado.calcular_area())
+print(cuadrado.calcular_perim())
